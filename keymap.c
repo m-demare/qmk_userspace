4fd433b215adbb87fb71785ff52dd48a36b654ba
Dema <34817965+m-demare@users.noreply.github.com>
2022-07-25 17:02:17 -0300

    fix: 'o' binding in vim layer goes back to insert

--- keymap.c
+++ keymap.c
@@ -22,8 +22,16 @@ enum custom_keycodes {
 #define KC_LBRCK KC_LBRACKET
 #define KC_RBRCK KC_RBRACKET
 
+enum layers {
+    QWERTY,
+    SYMBOLS,
+    VIMISH,
+    MOUSE_AND_MEDIA,
+    GAMING
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_moonlander(
+  [QWERTY] = LAYOUT_moonlander(
     KC_GRAVE ,  KC_1   ,  KC_2   ,  KC_3   ,  KC_4   ,  KC_5   ,  KC_NO  ,               KC_EQUAL ,  KC_6   ,  KC_7   ,   KC_8   ,  KC_9   ,  KC_0   ,  KC_MINUS,
     KC_DELETE,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,  KC_NO  ,               TG(1)    ,  KC_Y   ,  KC_U   ,   KC_I   ,  KC_O   ,  KC_P   , KC_BSLASH,
     KC_BSPACE,  KC_A   ,  KC_S   ,  KC_D   ,LT(2,KC_F), KC_G   ,  KC_NO  ,               KC_NO    ,  KC_H   ,  KC_J   ,   KC_K   ,  KC_L   ,LT(3,KC_SCOLON),MT(MOD_LGUI, KC_QUOTE),
@@ -31,7 +39,7 @@ WSFT(KC_ESC), WCTL(KC_Z), KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,                
     KC_LCTRL ,  KC_NO  ,  KC_NO  ,  KC_LEFT, KC_RIGHT,                     TG(2), TG(4)                     ,  KC_UP  ,  KC_DOWN ,  KC_NO  ,  KC_NO  , CAPS_WORD,
                                             KC_SPACE ,  KC_LALT,  KC_LGUI,               KC_RALT,  LT(1,KC_TAB),   KC_ENTER
   ),
-  [1] = LAYOUT_moonlander(
+  [SYMBOLS] = LAYOUT_moonlander(
     KC_ESC   ,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F5  ,  _______,               _______  ,  KC_F6  ,  KC_F7  ,  KC_F8  ,  KC_F9  ,  KC_F10 ,  KC_F11,
     _______  ,  KC_EXLM,  KC_AT  ,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,               _______  , KC_MINUS,  KC_7   ,  KC_8   ,  KC_9   ,  KC_ASTR,  KC_F12,
     _______  ,  KC_HASH,  KC_DLR ,  KC_LPRN,  KC_RPRN, KC_GRAVE,  _______,               _______  ,  KC_UNDS,  KC_4   ,  KC_5   ,  KC_6   , KC_EQUAL,KC_KP_PLUS,
@@ -39,7 +47,7 @@ WSFT(KC_ESC), WCTL(KC_Z), KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,                
     _______  , KC_COMMA,  _______,  RGB_VAD,  RGB_VAI,                   RGB_MOD, RGB_TOG                   ,  _______,  KC_0   ,  KC_DOT ,  _______, _______,
                                               _______,  _______,  _______,               _______, _______, _______
   ),
-  [2] = LAYOUT_moonlander(
+  [VIMISH] = LAYOUT_moonlander(
     _______  ,  _______,  _______,  _______,  _______,  _______,  _______,               _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______  ,  _______,ST_MACRO_0,C(KC_RIGHT),_______, _______,  _______,               _______,  _______,  _______,  TO(0)  ,ST_MACRO_1, _______, _______,
     _______  ,  TO(0)  ,  _______,C(KC_DELETE),_______, _______,  _______,               _______,  KC_LEFT,  KC_DOWN,  KC_UP  ,  KC_RIGHT, _______,  _______,
@@ -55,7 +63,7 @@ WSFT(KC_ESC), WCTL(KC_Z), KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,                
     _______  , _______,  _______,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,_______,_______                  ,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,_______,_______,
                                              _______  , _______,  _______,               _______,  _______,KC_WWW_BACK
   ),
-  [4] = LAYOUT_moonlander(
+  [GAMING] = LAYOUT_moonlander(
     KC_ESC   ,  _______,  _______,  _______,  _______,  _______,  _______,               _______,  _______,  _______,  _______,  _______,  _______,  _______,
     KC_TAB   ,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  _______,  _______,               _______,  _______,  _______,  _______,  _______,  _______,  _______,
     KC_CAPSLOCK,KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  _______,  _______,               _______,  _______,  _______,  _______,  KC_UP  ,  _______,  _______,
@@ -124,17 +132,17 @@ void rgb_matrix_indicators_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ST_MACRO_0:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_RIGHT)) SS_DELAY(100) SS_TAP(X_RIGHT));
-
-    }
-    break;
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL(SS_TAP(X_RIGHT)) SS_DELAY(100) SS_TAP(X_RIGHT));
+      }
+      break;
     case ST_MACRO_1:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_END) SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_END) SS_DELAY(100) SS_TAP(X_ENTER));
+        layer_move(QWERTY);
+        return false;
+      }
+      break;
     case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
