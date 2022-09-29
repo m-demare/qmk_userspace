05bcdf3ee5e0242c1cf93b2e01b3c96d4752f7df
Dema <34817965+m-demare@users.noreply.github.com>
2022-09-29 15:47:01 -0300

    chore: attempt to make my keymap more readable

--- keymap.c
+++ keymap.c
@@ -2,9 +2,9 @@
 #include "version.h"
 #include "keymap_us_international.h"
 #include "layers.h"
+#include "layout_mdemare.h"
 #include "features/compose.h"
 #include "features/vim_mode.h"
-
 
 // To avoid ccls complaining
 #include "dummy_includes.h"
@@ -12,64 +12,69 @@
 #define MOON_LED_LEVEL LED_LEVEL
 
 enum custom_keycodes {
-    VIM_W = ML_SAFE_RANGE,
-    VIM_O,
+    START = ML_SAFE_RANGE,
 };
 
 // Abbreviations
-#define CTLT(kc) MT(MOD_LCTL, KC_##kc)
-#define RCTLT(kc) MT(MOD_RCTL, KC_##kc)
-#define SFTT(key) (MT(MOD_LSFT, KC_##key))
-#define LGUIT(kc) MT(MOD_LGUI, KC_##kc)
-#define SYM SYMBOLS
-#define VIM VIMISH
-#define MY_SCLN LT(MOUSE_AND_MEDIA,KC_SCLN)
+#define KC_COMP COMPOSE
+#define KC_TG_SYM TG(SYMBOLS)
+#define KC_TG_VIM TG(VIMISH)
+#define KC_TG_GAM TG(GAMING)
 
 // Special keys
-#define MY_CARET LT(0, KC_CIRC)
+#define KC__CIRC LT(0, KC_CIRC)
+#define KC__QUOT LGUI_T(KC_QUOT)
+#define KC__TAB LT(SYMBOLS, KC_TAB)
+#define KC__Z CTL_T(KC_Z)
+#define KC__ESC SFT_T(KC_ESC)
+#define KC__F LT(VIMISH, KC_F)
+#define KC__SLSH RCTL_T(KC_SLSH)
+#define KC__SCLN LT(MOUSE_AND_MEDIA, KC_SCLN)
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [QWERTY] = LAYOUT_moonlander(
-        KC_GRAVE ,  KC_1   ,  KC_2   ,  KC_3   ,  KC_4   ,  KC_5   ,  XXXXXXX,               KC_EQUAL ,  KC_6   ,  KC_7   ,   KC_8  ,  KC_9   ,  KC_0   , KC_MINUS,
-        KC_DELETE,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,  XXXXXXX,               TG(SYM)  ,  KC_Y   ,  KC_U   ,   KC_I  ,  KC_O   ,  KC_P   ,  KC_BSLS,
-        KC_BSPACE,  KC_A   ,  KC_S   ,  KC_D   ,LT(VIM,KC_F),KC_G  ,  XXXXXXX,               XXXXXXX  ,  KC_H   ,  KC_J   ,   KC_K  ,  KC_L   ,MY_SCLN,LGUIT(QUOT),
-        SFTT(ESC),  CTLT(Z),  KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,                                     KC_N   ,  KC_M   , KC_COMMA,  KC_DOT ,RCTLT(SLSH),KC_RSFT,
-        KC_LCTRL ,  XXXXXXX,  XXXXXXX,  KC_LEFT, KC_RIGHT,                   TG(VIM), TG(GAMING)                ,  KC_UP  ,  KC_DOWN,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                                KC_SPACE ,  KC_LALT,  KC_LGUI,               COMPOSE,LT(SYM,KC_TAB),KC_ENTER
+    [QWERTY] = LAYOUT_moonlander_mdemare(
+        GRAVE,  1  ,  2  ,  3  ,  4  ,  5  , XXX ,               EQUAL,  6  ,  7  ,  8  ,  9  ,  0  ,MINUS,
+         DEL ,  Q  ,  W  ,  E  ,  R  ,  T  , XXX ,              TG_SYM,  Y  ,  U  ,  I  ,  O  ,  P  , BSLS,
+         BSPC,  A  ,  S  ,  D  ,  _F ,  G  , XXX ,                XXX ,  H  ,  J  ,  K  ,  L  ,_SCLN,_QUOT,
+         _ESC,  _Z ,  X  ,  C  ,  V  ,  B  ,                             N  ,  M  ,COMMA, DOT ,_SLSH, RSFT,
+        LCTRL,  XXX,  XXX, LEFT,RIGHT,          TG_VIM,     TG_GAM,            UP , DOWN, XXX , XXX , XXX ,
+                            SPC, LALT, LGUI,                            COMP, _TAB,ENTER
     ),
-    [VIMISH] = LAYOUT_moonlander(
-        _______  ,  _______,  _______,  _______,  _______,  _______,  _______,               _______  ,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______  ,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,  _______,               _______  ,  KC_Y   ,  KC_U   ,   KC_I  ,  KC_O   ,  KC_P   ,  _______,
-        _______  ,  KC_A   ,  KC_S   ,  KC_D   ,  _______,  KC_G   ,  _______,               _______  ,  KC_H   ,  KC_J   ,   KC_K  ,  KC_L   ,  _______,  _______,
-        _______  ,  CTLT(Z),  KC_X   ,  KC_C   ,  KC_V   ,  KC_B,                                        KC_N   ,  KC_M   , KC_COMMA,  KC_DOT ,  _______,  _______,
-        _______  ,  _______,  _______,  _______,  _______,                  _______, _______                    ,  _______,  _______,  _______,  _______,  _______,
-                                                  _______,  _______,  _______,               _______  ,  _______,  _______
+    [VIMISH] = LAYOUT_moonlander_mdemare(
+             ,     ,     ,     ,     ,     ,     ,                    ,     ,     ,     ,     ,     ,     ,
+             ,  Q  ,  W  ,  E  ,  R  ,  T  ,     ,                    ,  Y  ,  U  ,  I  ,  O  ,  P  ,     ,
+             ,  A  ,  S  ,  D  ,     ,  G  ,     ,                    ,  H  ,  J  ,  K  ,  L  ,     ,     ,
+             ,  _Z ,  X  ,  C  ,  V  ,  B  ,                             N  ,  M  ,COMMA, DOT ,     ,     ,
+             ,     ,     ,     ,     ,                  ,       ,                 ,     ,     ,     ,     ,
+                               ,     ,     ,                                ,     ,
     ),
-    [SYMBOLS] = LAYOUT_moonlander(
-        KC_ESC   ,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F5  ,  _______,               _______  ,  KC_F6  ,  KC_F7  ,  KC_F8  ,  KC_F9  ,  KC_F10 ,  KC_F11 ,
-        _______  ,  KC_EXLM,  KC_AT  ,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,               _______  , KC_MINUS,  KC_7   ,  KC_8   ,  KC_9   ,  KC_ASTR,  KC_F12 ,
-        _______  ,  KC_HASH,  KC_DLR ,  KC_LPRN,  KC_RPRN, KC_GRAVE,  _______,               _______  ,  KC_UNDS,  KC_4   ,  KC_5   ,  KC_6   ,  KC_EQL ,  KC_PPLS,
-        _______  ,  KC_PERC, MY_CARET,  KC_LBRC,  KC_RBRC,  KC_TILD,                                     KC_AMPR,  KC_1   ,  KC_2   ,  KC_3   ,  KC_BSLS,  _______,
-        _______  , KC_COMMA,  _______,  RGB_VAD,  RGB_VAI,                   RGB_MOD, RGB_TOG                   ,  _______,  KC_0   ,  KC_DOT ,  _______,  _______,
-                                                  _______,  _______,  _______,               _______  ,  _______,  _______
+    [SYMBOLS] = LAYOUT_moonlander_mdemare(
+        ESC  ,  F1 ,  F2 ,  F3 ,  F4 ,  F5 ,     ,                    ,  F6 ,  F7 ,  F8 ,  F9 , F10 , F11 ,
+             , EXLM,  AT , LCBR, RCBR, PIPE,     ,                    ,MINUS,  7  ,  8  ,  9  , ASTR, F12 ,
+             , HASH, DLR , LPRN, RPRN,GRAVE,     ,                    , UNDS,  4  ,  5  ,  6  , EQL , PPLS,
+             , PERC,_CIRC, LBRC, RBRC, TILD,                            AMPR,  1  ,  2  ,  3  , BSLS,     ,
+             ,COMMA,     ,RGBDN,RGBUP,             RGBMD,  RGBTG,                 ,  0  , DOT ,     ,     ,
+                               ,     ,     ,                                ,     ,
     ),
-    [MOUSE_AND_MEDIA] = LAYOUT_moonlander(
-        _______  ,  _______,  _______,  _______,  _______,  _______,  _______,               _______  ,  _______,  _______,  _______,  _______,  _______,    RESET,
-        _______  ,  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,  _______,               _______  ,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______  ,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  _______,               _______  ,  _______,  _______,  _______,  _______,  _______,  KC_MPLY,
-        _______  ,  _______,  _______,  _______,  _______,  _______,                                     _______,  _______,  _______,  _______,  _______,  _______,
-        _______  ,  _______,  _______,  KC_MPRV,  KC_MNXT,                   _______,_______                    ,  KC_VOLU,  KC_VOLD,  KC_MUTE,  _______,  _______,
-                                                _______  ,  _______,  _______,               _______  ,  KC_WBAK,  KC_WFWD
+    [MOUSE_AND_MEDIA] = LAYOUT_moonlander_mdemare(
+             ,     ,     ,     ,     ,     ,     ,                    ,     ,     ,     ,     ,     ,RESET,
+             ,     , BTN1, MS_U, BTN2, WH_U,     ,                    ,     ,     ,     ,     ,     ,     ,
+             ,     , MS_L, MS_D, MS_R, WH_D,     ,                    ,     ,     ,     ,     ,     , MPLY,
+             ,     ,     ,     ,     ,     ,                                ,     ,     ,     ,     ,     ,
+             ,     ,     , MPRV, MNXT,                  ,       ,             VOLU, VOLD, MUTE,     ,     ,
+                               ,     ,     ,                                , WBAK, WFWD
     ),
-    [GAMING] = LAYOUT_moonlander(
-        KC_ESC   ,  _______,  _______,  _______,  _______,  _______,  _______,               _______  ,  _______,  _______,  _______,  _______,  _______,  _______,
-        KC_TAB   ,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  _______,  _______,               _______  ,  _______,  _______,  _______,  _______,  _______,  _______,
-        KC_CAPS  ,  KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  _______,  _______,               _______  ,  _______,  _______,  _______,  KC_UP  ,  _______,  _______,
-        KC_LSHIFT,  KC_Z   ,  KC_X   ,  KC_C   ,  _______,  _______,                                     _______,  _______,  KC_LEFT,  KC_DOWN, KC_RIGHT,  _______,
-        KC_LCTRL ,  _______,  _______,  _______,  _______,                  _______, _______                    ,  _______,  _______,  _______,  _______,  _______,
-                                                KC_SPACE ,  _______,  _______,               _______  ,  _______,  _______
+    [GAMING] = LAYOUT_moonlander_mdemare(
+        ESC  ,     ,     ,     ,     ,     ,     ,                    ,     ,     ,     ,     ,     ,     ,
+        TAB  ,  Q  ,  W  ,  E  ,  R  ,     ,     ,                    ,     ,     ,     ,     ,     ,     ,
+        CAPS ,  A  ,  S  ,  D  ,  F  ,     ,     ,                    ,     ,     ,     ,  UP ,     ,     ,
+        LSFT ,  Z  ,  X  ,  C  ,     ,     ,                                ,     , LEFT, DOWN,RIGHT,     ,
+        LCTRL,     ,     ,     ,     ,                     ,       ,              ,     ,     ,     ,     ,
+                         SPACE ,     ,     ,                                ,     ,
     ),
 };
+// clang-format on
 
 extern rgb_config_t rgb_matrix_config;
 
@@ -77,6 +82,7 @@ void keyboard_post_init_user(void) {
     rgb_matrix_enable();
 }
 
+// clang-format off
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
     [VIMISH] = { {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {144,252,181}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {144,252,181}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {144,252,181}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {144,252,181}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210}, {89,252,210} },
 
@@ -87,6 +93,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
     [GAMING] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {185,219,233}, {0,221,226}, {185,219,233}, {0,0,0}, {0,0,0}, {0,221,226}, {0,221,226}, {185,219,233}, {0,0,0}, {0,0,0}, {185,219,233}, {0,221,226}, {185,219,233}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,221,226}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,221,226}, {0,0,0}, {0,0,0}, {0,0,0}, {0,221,226}, {0,221,226}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,221,226}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
 
 };
+// clang-format on
 
 void set_layer_color(int layer) {
     for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
@@ -96,11 +103,11 @@ void set_layer_color(int layer) {
             .v = pgm_read_byte(&ledmap[layer][i][2]),
         };
         if (!hsv.h && !hsv.s && !hsv.v) {
-            rgb_matrix_set_color( i, 0, 0, 0 );
+            rgb_matrix_set_color(i, 0, 0, 0);
         } else {
-            RGB rgb = hsv_to_rgb( hsv );
-            float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-            rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+            RGB   rgb = hsv_to_rgb(hsv);
+            float f   = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+            rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
         }
     }
 }
@@ -108,9 +115,9 @@ void set_layer_color(int layer) {
 void rgb_matrix_indicators_user(void) {
     if (keyboard_config.disable_layer_led) { return; }
     uint8_t layer = biton32(layer_state);
-    if(SYMBOLS == layer || VIMISH == layer || MOUSE_AND_MEDIA == layer || GAMING == layer){
+    if (SYMBOLS == layer || VIMISH == layer || MOUSE_AND_MEDIA == layer || GAMING == layer) {
         set_layer_color(layer);
-    } else if (rgb_matrix_get_flags() == LED_FLAG_NONE){
+    } else if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
         rgb_matrix_set_color_all(0, 0, 0);
     }
 }
@@ -127,7 +134,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(0, KC_CIRC):
             if (!record->tap.count && record->event.pressed) {
-                tap_code16(C(KC_6));    // On hold send ctrl-^ (buffer swap)
+                tap_code16(C(KC_6)); // On hold send ctrl-^ (buffer swap)
                 return false;
             }
             if (record->event.pressed) {
@@ -138,5 +145,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
-
---
+++ layout_mdemare.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// clang-format off
+#define LAYOUT_moonlander_mdemare( \
+    k00, k01, k02, k03, k04, k05, k06,   k60, k61, k62, k63, k64, k65, k66, \
+    k10, k11, k12, k13, k14, k15, k16,   k70, k71, k72, k73, k74, k75, k76, \
+    k20, k21, k22, k23, k24, k25, k26,   k80, k81, k82, k83, k84, k85, k86, \
+    k30, k31, k32, k33, k34, k35,             k91, k92, k93, k94, k95, k96, \
+    k40, k41, k42, k43, k44,      k53,   kb3,      ka2, ka3, ka4, ka5, ka6, \
+                        k50, k51, k52,   kb4, kb5, kb6 \
+) \
+LAYOUT_moonlander( \
+    KC_ ## k00, KC_ ## k01, KC_ ## k02, KC_ ## k03, KC_ ## k04, KC_ ## k05, KC_ ## k06,   KC_ ## k60, KC_ ## k61, KC_ ## k62, KC_ ## k63, KC_ ## k64, KC_ ## k65, KC_ ## k66, \
+    KC_ ## k10, KC_ ## k11, KC_ ## k12, KC_ ## k13, KC_ ## k14, KC_ ## k15, KC_ ## k16,   KC_ ## k70, KC_ ## k71, KC_ ## k72, KC_ ## k73, KC_ ## k74, KC_ ## k75, KC_ ## k76, \
+    KC_ ## k20, KC_ ## k21, KC_ ## k22, KC_ ## k23, KC_ ## k24, KC_ ## k25, KC_ ## k26,   KC_ ## k80, KC_ ## k81, KC_ ## k82, KC_ ## k83, KC_ ## k84, KC_ ## k85, KC_ ## k86, \
+    KC_ ## k30, KC_ ## k31, KC_ ## k32, KC_ ## k33, KC_ ## k34, KC_ ## k35,                           KC_ ## k91, KC_ ## k92, KC_ ## k93, KC_ ## k94, KC_ ## k95, KC_ ## k96, \
+    KC_ ## k40, KC_ ## k41, KC_ ## k42, KC_ ## k43, KC_ ## k44,             KC_ ## k53,   KC_ ## kb3,             KC_ ## ka2, KC_ ## ka3, KC_ ## ka4, KC_ ## ka5, KC_ ## ka6, \
+                                                    KC_ ## k50, KC_ ## k51, KC_ ## k52,   KC_ ## kb4, KC_ ## kb5, KC_ ## kb6 \
+)
+// clang-format on
+
+#define KC_     _______
+#define KC_XXX  XXXXXXX
+
+#define KC_RGBDN RGB_VAD
+#define KC_RGBUP RGB_VAI
+#define KC_RGBMD RGB_MOD
+#define KC_RGBTG RGB_TOG
+#define KC_RESET RESET
